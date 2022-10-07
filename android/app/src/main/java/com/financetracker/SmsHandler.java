0483ddc014dc8a1a9dae0c89872d9e9e193fbf55
Neeraj Sharma <neerajs@observe.ai>
2022-10-08 03:09:24 +0530

    implement fetch sms support

--- android/app/src/main/java/com/financetracker/SmsHandler.java
+++ android/app/src/main/java/com/financetracker/SmsHandler.java
@@ -6,13 +6,24 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.content.Context;
+import android.os.Bundle;
+
 import java.util.Map;
 import java.util.HashMap;
 import android.util.Log;
 
 public class SmsHandler extends ReactContextBaseJavaModule {
+
+  private static ReactApplicationContext reactContext;
+
    SmsHandler(ReactApplicationContext context) {
        super(context);
+       reactContext = context;
    }
 
   @Override
@@ -20,11 +31,15 @@ public class SmsHandler extends ReactContextBaseJavaModule {
     return "SmsHandler";
   }
 
-
   @ReactMethod
   public void getSmsName(String name, Promise promise) {
-   Log.d("CalendarModule", "Create event called with name: " + name);
-   String text = "Create event called with name: " + name;
-   promise.resolve(text);
+    Log.d("CalendarModule", "Create event called with name: " + name);
+    //  String text = "Create event called with name: " + name;
+    //  promise.resolve(text);
+
+    Cursor cursor = reactContext.getContentResolver().query(Uri.parse("content://sms"), null, null, null, null);
+    cursor.moveToFirst();
+
+    promise.resolve(cursor.getString(12));
   }
 }
